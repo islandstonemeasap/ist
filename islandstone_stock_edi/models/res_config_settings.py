1029188b65b75c1fda28dbeef054cfa36ee94313
cic-odoo <cic@odoo.com>
2020-03-13 11:46:00 -0700

    [REM] Comment out references to wayfair to push only build.com to prod

--- islandstone_stock_edi/models/res_config_settings.py
+++ islandstone_stock_edi/models/res_config_settings.py
@@ -7,8 +7,8 @@ from odoo import api, fields, models, _
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    email_wayfair = fields.Char(string='Wayfair Report Email', readonly=False,
-                                help='Set this email to auto send Product Inventory Report.')
+    # email_wayfair = fields.Char(string='Wayfair Report Email', readonly=False,
+    #                             help='Set this email to auto send Product Inventory Report.')
 
     email_build = fields.Char(string='Build.com Report Email', readonly=False,
                               help='Set this email to auto send Product Inventory Report.')
@@ -17,21 +17,21 @@ class ResCompany(models.Model):
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    ftp_host_wayfair = fields.Char(string='Host', default='',
-                                   config_parameter='islandstone_stock_edi.ftp_host_wayfair')
-    ftp_port_wayfair = fields.Integer(string='Port', required=True, default=22,
-                                      config_parameter='islandstone_stock_edi.ftp_port_wayfair')
-    ftp_protocol_wayfair = fields.Selection(selection=[('ftp', 'FTP - File Transfer Protocol'),
-                                                       ('sftp', 'SFTP - SSH File Transfer Protocol')],
-                                            config_parameter='islandstone_stock_edi.ftp_protocol_wayfair',
-                                            string='Protocol', required=True, default='ftp')
-    ftp_login_wayfair = fields.Char(string='Username', required=True, default='',
-                                    config_parameter='islandstone_stock_edi.ftp_login_wayfair')
-    ftp_password_wayfair = fields.Char(string='Password', required=True, default='',
-                                       config_parameter='islandstone_stock_edi.ftp_password_wayfair')
-
-    email_wayfair = fields.Char(string='Wayfair Report Email', related='company_id.email_wayfair', readonly=False,
-                                help='Set this email to auto send Product Inventory Report.')
+    # ftp_host_wayfair = fields.Char(string='Host', default='',
+    #                                config_parameter='islandstone_stock_edi.ftp_host_wayfair')
+    # ftp_port_wayfair = fields.Integer(string='Port', required=True, default=22,
+    #                                   config_parameter='islandstone_stock_edi.ftp_port_wayfair')
+    # ftp_protocol_wayfair = fields.Selection(selection=[('ftp', 'FTP - File Transfer Protocol'),
+    #                                                    ('sftp', 'SFTP - SSH File Transfer Protocol')],
+    #                                         config_parameter='islandstone_stock_edi.ftp_protocol_wayfair',
+    #                                         string='Protocol', required=True, default='ftp')
+    # ftp_login_wayfair = fields.Char(string='Username', required=True, default='',
+    #                                 config_parameter='islandstone_stock_edi.ftp_login_wayfair')
+    # ftp_password_wayfair = fields.Char(string='Password', required=True, default='',
+    #                                    config_parameter='islandstone_stock_edi.ftp_password_wayfair')
+    #
+    # email_wayfair = fields.Char(string='Wayfair Report Email', related='company_id.email_wayfair', readonly=False,
+    #                             help='Set this email to auto send Product Inventory Report.')
 
     email_build = fields.Char(string='Build.com Report Email', related='company_id.email_build', readonly=False,
                               help='Set this email to auto send Product Inventory Report.')
