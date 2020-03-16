0aedd80d12e8923469f115dbf4172831ff78af15
Prashant Panchal <ppa@odoo.com>
2020-03-16 18:52:12 +0530

    [ADD] created the xml file when validate the order and when validate multipal then it will be created a zip file, and when cancel the invoice then xml file it will be deleted

--- invoice_edi/models/account_invoice.py
+++ invoice_edi/models/account_invoice.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import base64
+import zipfile
+import io
 from odoo import api, models, _
+import logging
+from odoo.exceptions import ValidationError
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountInvoice(models.Model):
@@ -11,14 +17,17 @@ class AccountInvoice(models.Model):
     def action_invoice_open(self):
         res = super(AccountInvoice, self).action_invoice_open()
         sale_order = self.env['sale.order']
-        for invoice in self.filtered(lambda x: x.type == 'out_invoice'):
+        attachments = []
+        Ir_Attachment = self.env['ir.attachment']
+        invoices = self.filtered(lambda x: x.type == 'out_invoice')
+        for invoice in invoices:
             if invoice.origin:
                 sale_order = sale_order.search([('name', '=', invoice.origin)], limit=1)
             data = self.env['ir.qweb'].render('invoice_edi.invoice_to_xml', {'invoice': invoice, 'order': sale_order})
             data = b'<?xml version="1.0" encoding="utf-8"?>' + data
-            filename = ('invoice-%s.xml' % invoice.number.replace('/', '_'))
+            filename = ('%s.xml' % invoice.number.replace('/', '_'))
             ctx = self.env.context.copy()
-            attachment_id = self.env['ir.attachment'].with_context(ctx).create({
+            attachment_id = Ir_Attachment.with_context(ctx).create({
                 'name': filename,
                 'res_id': invoice.id,
                 'res_model': invoice._name,
@@ -26,5 +35,42 @@ class AccountInvoice(models.Model):
                 'datas_fname': filename,
                 'description': _(invoice.number),
                 })
-            invoice.message_post(body=_('TESTINIG'), attachments=[attachment_id])
+            attachments.append(attachment_id)
+            invoice.message_post(body=_('Invoice File has been created, So please check it Attachment'), attachments=[attachment_id])
+        if len(attachments) > 1:
+            stream = self._create_zip(attachments)
+            for invoice in invoices:
+                Ir_Attachment.with_context(ctx).create({
+                    'name': 'filename_invoice_zip',
+                    'res_id': invoice.id,
+                    'res_model': invoice._name,
+                    'datas': base64.encodestring(stream),
+                    'datas_fname': 'filename_invoice_zip',
+                    'description': _(invoice.number),
+                    })
+        return res
+
+    def _create_zip(self, attachments):
+        stream = io.BytesIO()
+        try:
+            with zipfile.ZipFile(stream, 'w') as doc_zip:
+                for attachment in attachments:
+                    if attachment.type in ['url', 'empty']:
+                        continue
+                    filename = attachment.datas_fname
+                    doc_zip.writestr(filename, base64.b64decode(attachment['datas']),
+                                     compress_type=zipfile.ZIP_DEFLATED)
+        except zipfile.BadZipfile:
+            _logger.exception("BadZipfile exception")
+            raise ValidationError(_('BadZipfile exception'))
+        return stream.getvalue()
+
+    @api.multi
+    def action_invoice_cancel(self):
+        try:
+            numbers = [('%s.xml' % i.number.replace('/', '_')) for i in self]
+            res = super(AccountInvoice, self).action_invoice_cancel()
+            self.env['ir.attachment'].search([('name', 'in', numbers), ('res_model', '=', 'account.invoice')]).unlink()
+        except Exception as e:
+            raise ValidationError(e)
         return res
