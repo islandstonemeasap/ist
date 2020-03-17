7cf4ace338109a3127cc1ce61bed720434a56681
ehe <ehe@odoo.com>
2020-03-17 20:29:43 +0000

    [ADD] additional requirement on third party account handling

--- ist_delivery/models/sale_order.py
+++ ist_delivery/models/sale_order.py
@@ -8,16 +8,24 @@ from odoo.tools import float_compare, float_round
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    delivery_payment_type = fields.Selection([('sender', 'Prepaid'), ('receiver', 'Collect'), ('third_party', '3rd Party')], string='Delivery Payment Type (Easypost)')
     # available delivery accounts according to partner
-    delivery_account_ids = fields.Many2many('partner.delivery.account', related="partner_id.delivery_account_ids", readonly=True)
+    # honestly, not sure this is all necessary since easypost only cares about country code and zip
+    partner_delivery_billing_ids = fields.Many2many('res.partner', related='partner_id.partner_delivery_billing_ids')
+    partner_delivery_billing_id = fields.Many2one('res.partner', ondelete='set null', string='3rd Party Billing Address')
+    delivery_account_ids = fields.Many2many('partner.delivery.account', compute='_compute_delivery_account_ids', store=True)
     delivery_account_id = fields.Many2one('partner.delivery.account', ondelete='set null', string='Delivery Account', states={'sale': [('readonly', True)]})
-    delivery_payment_type = fields.Selection([('sender', 'SENDER'), ('receiver', 'RECEIVER')], string='Delivery Payment Type (Easypost)')
-
-    @api.onchange('delivery_account_id')
-    def _onchange_delivery_account_id(self):
+    
+    @api.depends('delivery_payment_type', 'partner_id', 'partner_id.delivery_account_ids', 'partner_delivery_billing_id', 'partner_delivery_billing_id.delivery_account_ids')
+    def _compute_delivery_account_ids(self):
         for order in self:
-            if order.delivery_account_id:
-                order.delivery_payment_type = 'receiver'
-            else:
-                order.delivery_payment_type = 'sender'
+            account_ids = []
+            if order.delivery_payment_type == 'receiver':
+                account_ids = order.partner_id.delivery_account_ids.ids
+            elif order.delivery_payment_type == 'third_party' and order.partner_delivery_billing_id:
+                account_ids = order.partner_delivery_billing_id.delivery_account_ids.ids
+            order.delivery_account_ids = [(6, 0, account_ids)]
+
+
+
     
